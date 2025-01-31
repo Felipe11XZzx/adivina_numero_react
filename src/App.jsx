91c93410408d930080a02ca83985ffcaa8ef85e2
FelipeOrtiz <ortiz15243u@gmail.com>
2025-01-31 14:27:19 +0100

    Implementando logica del proyecto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import "./App.css";
+// Son las que vamos a utilizar dentro del navegador.
+// Esto es un react hook ('Investigar que es').
+import { useRef, useState } from "react";
+
+const number = Math.trunc(Math.random() * 20) + 1;
 
 function App() {
+  // Variables de estado.
+  const [score, setScore] = useState(20);
+  const [highscore, setHighscore] = useState(0);
+  const inputRef = useRef(null);
+
+  const handleCheck = () => {
+    // Comprobar si el valor introducido es igual al número aleatorio.
+    const inputNumber = Number(inputRef.current.value);
+    if (inputNumber === number) {
+      document.querySelector(".number").textContent = number;
+    }
+    console.log(inputRef.current.value);
+    setScore(score - 1);
+  };
+
   return (
     <>
       <header>
@@ -11,16 +31,18 @@ function App() {
       </header>
       <main>
         <section className="left">
-          <input type="number" className="guess" />
-          <button className="btn check">Check!</button>
+          <input type="number" className="guess" ref={inputRef} />
+          <button className="btn check" onClick={handleCheck}>
+            Check!
+          </button>
         </section>
         <section className="right">
           <p className="message">Start guessing...</p>
           <p className="label-score">
-            💯 Score: <span className="score">20</span>
+            💯 Score: <span className="score">{score}</span>
           </p>
           <p className="label-highscore">
-            🥇 Highscore: <span className="highscore">0</span>
+            🥇 Highscore: <span className="highscore">{highscore}</span>
           </p>
         </section>
       </main>
